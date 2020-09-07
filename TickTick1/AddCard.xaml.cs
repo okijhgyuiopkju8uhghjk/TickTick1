67a3003a792ddd0b24f999547019d71537692a93
Srinivas Adapa <994984@gmail.com>
2020-09-07 10:44:47 +0530

    first test

---
+++ TickTick1/AddCard.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TickTick1
+{
+    /// <summary>
+    /// Interaction logic for AddCard.xaml
+    /// </summary>
+    public partial class AddCard : Window
+    {
+        public AddCard()
+        {
+            InitializeComponent();
+        }
+
+        private void Back_Click(object sender, RoutedEventArgs e)
+        {
+            CardSet objcardSet = new CardSet();
+            //this.Visibility = Visibility.Hidden; //hiding the current window
+            objcardSet.Show();
+        }
+    }
+}
---
+++ TickTick1/CardSet.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TickTick1
+{
+    /// <summary>
+    /// Interaction logic for CardSet.xaml
+    /// </summary>
+    public partial class CardSet : Window
+    {
+        public CardSet()
+        {
+            InitializeComponent();
+        }
+
+        private void home_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow objmainwindow = new MainWindow();
+            //this.Visibility = Visibility.Hidden; //hiding the current window
+            objmainwindow.Show();
+        }
+
+        private void add_Click(object sender, RoutedEventArgs e)
+        {
+            AddCard objaddcard = new AddCard();
+            objaddcard.Show();
+        }
+    }
+}
